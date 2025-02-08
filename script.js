f96c81a8bda4750926e2fea04db84f395e744803
briencheow <44813658+Briencheow@users.noreply.github.com>
2025-02-09 00:41:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -12,12 +12,21 @@ const messages = [
 ];
 
 let messageIndex = 0;
+let noClickCount = 0;
 
+const imagePaths = [
+    "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExbW5lenZyZHI5OXM2eW95b3pmMG40cWVrMDhtNjVuM3A4dGNxa2g2dSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/VM1fcpu2bKs1e2Kdbj/giphy.gif",
+    "https://media.giphy.com/media/aU1zEDJ9xPVjFBbtvJ/giphy.gif?cid=ecf05e475iel9bang2qbef8m6ielasiswu3egon6kt2u28fm&ep=v1_stickers_related&rid=giphy.gif&ct=s",
+    "https://media.giphy.com/media/MFkTITj69pMOPlbfeX/giphy.gif?cid=ecf05e475iel9bang2qbef8m6ielasiswu3egon6kt2u28fm&ep=v1_stickers_related&rid=giphy.gif&ct=s",
+    "https://media.giphy.com/media/kcyx4ajxyWRelw1uPJ/giphy.gif?cid=ecf05e475iel9bang2qbef8m6ielasiswu3egon6kt2u28fm&ep=v1_stickers_related&rid=giphy.gif&ct=s"
+];
 
 function handleNoClick() {
+    noClickCount++;
     const noButton = document.querySelector('.no-button');
     const yesButton = document.querySelector('.yes-button');
     noButton.textContent = messages[messageIndex];
+    imageDisplay.src = imagePaths[noClickCount];
     messageIndex = (messageIndex + 1) % messages.length;
     const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
     yesButton.style.fontSize = `${currentSize * 1.5}px`;
