64170e084c35e2c3b8f64160660d13fac7db2983
briencheow <44813658+Briencheow@users.noreply.github.com>
2025-02-09 01:15:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,13 @@ const imagePaths = [
     "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExbW5lenZyZHI5OXM2eW95b3pmMG40cWVrMDhtNjVuM3A4dGNxa2g2dSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/VM1fcpu2bKs1e2Kdbj/giphy.gif",
     "https://media.giphy.com/media/aU1zEDJ9xPVjFBbtvJ/giphy.gif?cid=ecf05e475iel9bang2qbef8m6ielasiswu3egon6kt2u28fm&ep=v1_stickers_related&rid=giphy.gif&ct=s",
     "https://media.giphy.com/media/MFkTITj69pMOPlbfeX/giphy.gif?cid=ecf05e475iel9bang2qbef8m6ielasiswu3egon6kt2u28fm&ep=v1_stickers_related&rid=giphy.gif&ct=s",
-    "https://media.giphy.com/media/kcyx4ajxyWRelw1uPJ/giphy.gif?cid=ecf05e475iel9bang2qbef8m6ielasiswu3egon6kt2u28fm&ep=v1_stickers_related&rid=giphy.gif&ct=s"
+    "https://media.giphy.com/media/kcyx4ajxyWRelw1uPJ/giphy.gif?cid=ecf05e475iel9bang2qbef8m6ielasiswu3egon6kt2u28fm&ep=v1_stickers_related&rid=giphy.gif&ct=s",
+    "https://media.giphy.com/media/dnSDhYT5Y9a7yvtwmU/giphy.gif?cid=ecf05e475iel9bang2qbef8m6ielasiswu3egon6kt2u28fm&ep=v1_stickers_related&rid=giphy.gif&ct=s",
+    "https://media.giphy.com/media/dEZKOYuY4oQhMOZGEU/giphy.gif?cid=ecf05e475iel9bang2qbef8m6ielasiswu3egon6kt2u28fm&ep=v1_stickers_related&rid=giphy.gif&ct=s",
+    "https://media.giphy.com/media/2weSkZg9hvQW5Zv2fk/giphy.gif?cid=ecf05e475iel9bang2qbef8m6ielasiswu3egon6kt2u28fm&ep=v1_stickers_related&rid=giphy.gif&ct=s",
+    "https://media.giphy.com/media/27rspbAAESlzzFtN2e/giphy.gif?cid=ecf05e47ri1fh0zhau2lzed6yviio7psco8ifxwn292bss1i&ep=v1_stickers_related&rid=giphy.gif&ct=s",
+    "https://media.giphy.com/media/QlQdLBS70XJcZY1fLF/giphy.gif?cid=ecf05e47ri1fh0zhau2lzed6yviio7psco8ifxwn292bss1i&ep=v1_stickers_related&rid=giphy.gif&ct=s",
+    "https://media.giphy.com/media/45314GJC3sIVJWP0S1/giphy.gif?cid=ecf05e47ri1fh0zhau2lzed6yviio7psco8ifxwn292bss1i&ep=v1_stickers_related&rid=giphy.gif&ct=s"
 ];
 
 function handleNoClick() {
